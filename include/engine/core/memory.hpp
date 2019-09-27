f7466eddd5a3f84c967e24a27422c9afd0ba0df3
forenoonwatch <91013521+forenoonwatch@users.noreply.github.com>
2019-09-27 00:13:36 -0400

    Added <memory> classes to Memory header

--- include/engine/core/memory.hpp
+++ include/engine/core/memory.hpp
@@ -1,8 +1,18 @@
 #pragma once
 
+#include <memory>
 #include <cstring>
 
 namespace Memory {
+	template <typename T>
+	using unique_ptr = std::unique_ptr<T>;
+
+	template <typename T>
+	using shared_ptr = std::shared_ptr<T>;
+
+	template <typename T>
+	using weak_ptr = std::weak_ptr<T>;
+
 	FORCEINLINE void* memcpy(void* dest, const void* src, uintptr amt) {
 		return std::memcpy(dest, src, amt);
 	}
@@ -17,5 +27,15 @@ namespace Memory {
 
 	FORCEINLINE void* memset(void* dest, int value, uintptr amt) {
 		return std::memset(dest, value, amt);
+	}
+
+	template <typename T, typename... Args>
+	FORCEINLINE unique_ptr<T> make_unique(Args&&... args) {
+		return std::make_unique<T>(args...);
+	}
+
+	template <typename T, typename... Args>
+	FORCEINLINE shared_ptr<T> make_shared(Args&&... args) {
+		return std::make_shared<T>(args...);
 	}
 };
